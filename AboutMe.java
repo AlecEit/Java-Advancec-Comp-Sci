63d352a5a3a0072c2a913e233ca106d27ee8e2f8
AlecEit <alec.eitner@gmail.com>
2026-08-28 14:42:40 -0700

    8/28/26
    8/28/26

---
+++ AboutMe.java
@@ -0,0 +1,12 @@
+
+public class AboutMe{
+
+public static void main(String[] args) {
+    System.out.println("Name: Alec Eitner");
+    System.out.println("Birthday: May 18, 2026");
+    System.out.println("Hobbies: Robotics, videogames, Archery");
+    System.out.println("Favorite Book: Percy Jackson");
+    System.out.println("Favorite Movie: The Odyssey");
+}
+
+} 
---
+++ BlockLetters.java
@@ -0,0 +1,18 @@
+
+public class BlockLetters{
+
+public static void main(String[] args) {
+    System.out.println(" A ");
+    System.out.println("A A");
+    System.out.println("AAA");
+    System.out.println("A A");
+    System.out.println("A A");
+    System.out.println("\nEEE");
+    System.out.println("E");
+    System.out.println("E");
+    System.out.println("E");
+    System.out.println("EEE");
+
+}
+
+} 
